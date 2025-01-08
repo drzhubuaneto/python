7d16dbd0c9487f9ad8e993a77814d0c967bee62f
zelinane <zelinane@cvut.cz>
2025-01-08 08:41:05 +0100

    omg asi hotovo

--- sj_idk.py/haha.py
+++ sj_idk.py/haha.py
@@ -1,26 +1,30 @@
-import kagglehub  #načítání data setu z kaggle
+import kagglehub  #načítání datasetu z kaggle
 import matplotlib.pyplot as plt  #knihovna pro tvorbu grafů
-import pandas as pd  #práce s tabulkami
-import seaborn as sns
+import pandas as pd  #knihovna pro práci s tabulkami a daty
+import seaborn as sns  #knihovna pro vizualizace s porkočilými grafy
 
+#stažení datasetu z kaggle a načtení cesty k souboru
 path = kagglehub.dataset_download("hopesb/student-depression-dataset")
 #print("Path to dataset files:", path)
-#kontrola path
+#kontrola správného načtení cesty
 
+#načtení csv souboru s daty
 csv_file = f"{path}/Student Depression Dataset.csv" 
 data = pd.read_csv(csv_file)
 #print(data.head())
-#kontrola, že to načítá soubor
+#kontrola pár řádků, že to načítá soubor s daty
 
-#1) DISTRIBUCE PODLE POHLAVÍ A DEPRESE
-#výpočet procentuálního zastoupení deprese u žen a u mužů v datasetu
+#1) DISTRIBUCE PODLE POHLAVÍ + DEPRESE
+#výpočet počtů a procentuálního zastoupení deprese pro jednotlivá pohlaví v datasetu
 group_counts = data.groupby(['Gender', 'Depression']).size().reset_index(name='Count')
 total_counts = group_counts.groupby('Gender')['Count'].transform('sum')
 group_counts['Percentage'] = (group_counts['Count'] / total_counts * 100).round(1)
 
+#sloupcový graf s daty
 plt.figure(figsize=(8, 6))
 ax = sns.barplot(data=group_counts, x='Gender', y='Count', hue='Depression', palette='Set2')
-#procenta přiřadit ke grafu
+
+#přiřazení procentuálních hodnot nad sloupce
 for container in ax.containers:
     for bar, label in zip(container, container.datavalues):
         height = bar.get_height()
@@ -28,42 +32,40 @@ for container in ax.containers:
         ax.text(
             bar.get_x() + bar.get_width() / 2, height, f'{percentage}%', ha='center', va='bottom', fontsize=10
         )
-#legenda
+#upravení legendy a popisků v legendě
 handles, labels = ax.get_legend_handles_labels()
 custom_labels = ['No Depression', 'Depression']
 ax.legend(handles=handles, labels=custom_labels, title='Depression')
-
-#popisky grafu
 plt.title('Gender Distribution with Depression Factor')
 plt.xlabel('Gender')
 plt.ylabel('Count')
 plt.show()
 
-#2) DISTRIBUCE PODLE VĚKU A DEPRESE
-#vytvoření skupin dle věku
-bins = [15, 20, 25, 30, 100]
-labels = ['15-20', '21-25', '26-30', '31+']
+#2) DISTRIBUCE PODLE VĚKU + DEPRESE
+#vytvoření skupin a jejich rozdělení podle věku
+bins = [15, 20, 25, 30, 100] #intrvaly skupin
+labels = ['15-20', '21-25', '26-30', '31+'] #názvy skupin
 data['Age Group'] = pd.cut(data['Age'], bins=bins, labels=labels, right=False)
 
-#rozdělení dat 
+#rozdělení dat (takový jako přiřazení) dle věkových karegorií a deprese 
 grouped_data = data.groupby(['Age Group', 'Depression']).size().reset_index(name='Count')
 
+#sloupcový graf, úprava legendy a popisků grafu
 plt.figure(figsize=(10, 6))
 ax = sns.barplot(data=grouped_data, x='Age Group', y='Count', hue='Depression', palette='Set2')
-
 handles, labels = ax.get_legend_handles_labels()
 custom_labels = ['No Depression', 'Depression']
 ax.legend(handles=handles, labels=custom_labels, title='Depression')
-
 plt.title('Age Distribution by Depression Factor')
 plt.xlabel('Age Group')
 plt.ylabel('Count')
 plt.show()
 
 #3) SPÁNEK A DEPRESE
+#odstranění chybějících hodnot u sleep duration
 data = data.dropna(subset=['Sleep Duration'])
 
-#plots
+#sloupcový graf a úprava pořadí názvů sloupců
 sns.countplot(x='Sleep Duration', hue='Depression', data=data, palette='viridis', order=[
     "Less than 5 hours", "5-6 hours", "7-8 hours", "More than 8 hours"
 ])
@@ -73,8 +75,8 @@ plt.ylabel("Count")
 plt.legend(title="Depression")
 plt.show()
 
-
-#4) HISTORIE MENTAL ILLNESS V RODINĚ A DEPRESE
+#4) HISTORIE MENTAL ILLNESS V RODINĚ + DEPRESE
+#zase tvorba grafu
 sns.countplot(x='Family History of Mental Illness', hue='Depression', data=data, palette='Set2')
 plt.title("Family History of Mental Illness and Depression")
 plt.xlabel("Family History of Mental Illness")
@@ -82,22 +84,21 @@ plt.ylabel("Count")
 plt.legend(title="Depression")
 plt.show()
 
-#5) BOXPLOT NA ACADEMIC PRESSURE A DEPRESI
+#5) BOXPLOT NA ACADEMIC PRESSURE V ZÁVISLOSTI NA DEPRESI
 sns.boxplot(x='Depression', y='Academic Pressure', data=data, palette='coolwarm')
 plt.title("Academic Pressure by Depression")
 plt.show()
 
-#6) BOXPLOT NA FINANCIAL STRESS A DEPRESI
+#6) BOXPLOT NA FINANCIAL STRESS  V ZÁVISLOSTI NA DEPRESI
 sns.boxplot(x='Depression', y='Financial Stress', data=data, palette='coolwarm')
 plt.title("Financial Stress by Depression")
 plt.show()
 
-
-#7) academic pressure + study satisfaction and depression
+#7) ACADEMIC PRESSURE, STUDY SATISFACTION VS DEPRESE
 sns.scatterplot(
     x='Academic Pressure', 
     y='Study Satisfaction', 
-    hue='Depression',  # Add this to color points by Depression
+    hue='Depression',
     data=data, 
     palette='coolwarm'
 )
